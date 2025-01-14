1823c93475533dd5cb8c7c21405d3448a66a1a3d
xritik <singhritik00901@gmail.com>
2025-01-14 17:51:07 +0530

    IQuiz creation complete css page added

--- creater_side/src/components/IQuizCreationPage.js
+++ creater_side/src/components/IQuizCreationPage.js
@@ -5,7 +5,76 @@ const IQuizCreationPage = () => {
   return (
 
     <div className='iquizCreationSection'>
-        hello
+        <div className='iquizCreationPart1'>
+            <div className='questions'>Question 1</div>
+            <div className='questions'>Question 2</div>
+            <div className='questions'>Question 3</div>
+            <div className='addMoreBtn'>Add more</div>
+        </div>
+        <div className='iquizCreationPart2'>
+            <form className='addingIQuizForm'>
+                <div className='title_button'>
+                    {/* <div className='part1'></div> */}
+                    <div className='part1'>
+                        <input
+                            type='text'
+                            required
+                            placeholder='Type your question'
+                        />
+                    </div>
+                    <div className='part2'>
+                        <button>Save</button>
+                    </div>
+                </div>
+                <div className='question_timer'>
+                    <div className='part1'>1</div>
+                    <div className='part2'>
+                        <textarea
+                            type='text'
+                            required
+                            placeholder='Type your question'
+                        />
+                    </div>
+                    <div className='part3'>
+                        <select>
+                            <option>10 second</option>
+                            <option>20 second</option>
+                            <option>30 second</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='options'>
+                    <div className='option'>
+                        <div>
+                            <textarea 
+                                required 
+                                placeholder='Enter your first option'
+                            />
+                        </div>
+                        <div>
+                            <textarea 
+                                required 
+                                placeholder='Enter your second option'
+                            />
+                        </div>
+                    </div>
+                    <div className='option'>
+                        <div>
+                            <textarea 
+                                required 
+                                placeholder='Enter your third option'
+                            />
+                        </div>
+                        <div>
+                            <textarea 
+                                required 
+                                placeholder='Enter your fourth option'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
   )
 }
