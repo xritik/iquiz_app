a7d25509e92a7c236fc8bfbfdcd12a0458836879
xritik <singhritik00901@gmail.com>
2025-02-02 23:36:34 +0530

    schema created for runningIQuiz

---
+++ backend/models/runningIQuiz.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const runningIQuizSchema = new mongoose.Schema({
+    hoster: {
+        type: String,
+        required: true,
+    },
+    pin: {
+        type: Number,
+        required: true,
+    },
+    iquizId: {
+        type: String,
+        required: true,
+    },
+    players: [
+        {
+            name: { type: String, required: true },
+            answers: { type: [Boolean], default: [] },
+        }
+    ],
+    shownQuestionId: {
+        type: String
+    },
+    status: {
+        type: String,
+        default: 'Stop',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 3600,
+    }
+});
+
+const RunningIQuiz = mongoose.model('RunningIQuiz', runningIQuizSchema);
+module.exports = RunningIQuiz;
