4f4dc4c406062dce29af2a144a1b95b8a74a08c9
xritik <singhritik00901@gmail.com>
2025-04-06 09:13:12 +0530

    some changes

--- frontend/src/components/NotFoundPage.js
+++ frontend/src/components/NotFoundPage.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import '../css/notFound.css'
-import image from '../imgs/404-page-not-found.png'
+import React from 'react';
+import '../css/notFound.css';
+import image from '../imgs/404-page-not-found.png';
+import {Link} from 'react-router-dom';
+
 const NotFoundPage = ({ navigate }) => {
   return (
     <div className='notFoundSection'>
       <i className='bx bx-arrow-back back_arrow' onClick={() => {navigate('/')}}></i>
       <img className='pageNotFoundImage' src={image} alt='img'/>
       <div className='pageNotFoundText'>Page Not Found!</div>
+      <Link to={'/'}>Back</Link>
     </div>
   )
 }
